cff24a09a0f16fd17e1980eef6b49f950c03c197
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2024-01-16 01:53:59 -0300

    uma ideia que talvez funcione

--- src/view.js
+++ src/view.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 export function View({ estado = 0 }) {
   const [userEmail, setUserEmail] = useState('');
@@ -18,24 +17,7 @@ export function View({ estado = 0 }) {
         setUserEmail(email);
         setSerUid(userid);
       } else {
-        toast.info(`Usuario não autenticado`, {
-          position: toast.POSITION.TOP_CENTER,
-          autoClose: 1000,
-          closeButton: false,
-          tema: "red",
-          style: {
-            backgroundColor: '#780000',
-            color: '#BFDBF7',
-            width: '600px',
-            height: '400px',
-            fontSize: '60px',
-            textAlign: 'center',
-          },
-        });
-
-        setTimeout(() => {
           navigate('../login');
-        }, 2000);
       }
     });
     return () => unsubscribe();
